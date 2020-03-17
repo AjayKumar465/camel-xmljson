f6c8de9f612c140b957cc9afa7cecc8fccb00529
ajaykumark@ibaseit.com <ajaykumark@ibaseit.com>
2020-03-17 13:05:14 +0530

    initial commit

---
+++ src/main/java/com/camelxml/XML2JSONRoute.java
@@ -0,0 +1,18 @@
+package com.camelxml;
+import org.apache.camel.Route;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.xmljson.XmlJsonDataFormat;
+public class XML2JSONRoute extends RouteBuilder {
+    public void configure() throws Exception {
+        from("direct:marshalEmployeexml2json")
+                .to("log:?level=INFO&showBody=true")
+                .marshal().xmljson()
+                .to("log:?level=INFO&showBody=true");
+        final XmlJsonDataFormat xmlJsonFormat = new XmlJsonDataFormat();
+        xmlJsonFormat.setRootName("Employee");
+        from("direct:unMarshalEmployeejson2xml")
+                //.unmarshal().xmljson()
+                .unmarshal(xmlJsonFormat)
+                .to("log:?level=INFO&showBody=true");
+    }
+}
